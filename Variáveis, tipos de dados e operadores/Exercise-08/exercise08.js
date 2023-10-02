03ce09aecc7b504ee3a585b7c0d0e18e308f352e
= <lyulucca@gmail.com>
2023-10-02 10:22:03 -0300

    Feat: Completed exercise 08 and Changes in folder organization

---
+++ Variáveis, tipos de dados e operadores/Exercise-08/exercise08.js
@@ -0,0 +1,10 @@
+let numX = 0;
+
+//Faremos 5 incrementacoes com ++, ou seja, pegamos o valor da variavel e adicionamos (+1) cinco vezes
+numX++
+numX++
+numX++
+numX++
+numX++
+
+console.log('Antes o valor de numX era 0, agora o valor de numX e: ' + numX);
