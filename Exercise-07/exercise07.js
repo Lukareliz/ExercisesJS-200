0d4f123cf9bf66d3ae1ceabeda8773deeeb0e21b
= <lyulucca@gmail.com>
2023-10-02 09:50:03 -0300

    Feat: Completed exercise 07

---
+++ Exercise-07/exercise07.js
@@ -0,0 +1,9 @@
+let numX = 1;
+let numY = 2;
+let numZ = 3;
+
+console.log('NumX e maior que numY? ' + (numX > numY));
+console.log('NumX e menor que numY? ' + (numX < numY));
+console.log('NumX e maior ou igual a numZ? ' + (numX >= numZ));
+console.log('NumX e menor ou igual a numZ? ' + (numX <= numZ));
+console.log('NumZ e igual a numY? ' + (numZ == numY));
