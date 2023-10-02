2ef44f3c1c15edcb6b15d7a33e6a64f6448dbdcc
= <lyulucca@gmail.com>
2023-10-02 09:35:58 -0300

    Feat: Completed exercise 06

---
+++ Exercise-06/exercise06.js
@@ -0,0 +1,6 @@
+let booleanX = true;
+let booleanY = false;
+
+console.log('Porta logica AND: '+ (booleanX && booleanY));
+console.log('Porta logica OR: '+ (booleanX || booleanY));
+console.log('Porta logica NOT: '+ (booleanX != booleanY));
