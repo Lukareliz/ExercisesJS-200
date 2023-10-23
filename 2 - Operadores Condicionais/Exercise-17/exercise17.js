4b85cf96ae603607e95620d8dfd6616be0db1213
= <lyulucca@gmail.com>
2023-10-23 00:12:30 -0300

    Feat: Completed exercise 17

---
+++ 2 - Operadores Condicionais/Exercise-17/exercise17.js
@@ -0,0 +1,12 @@
+function verificarParOuImpar(num){
+    if(num % 2 == 0){
+        console.log('O número ' + num + ' é par.');
+    }else{
+        console.log('O número ' + num + ' é ímpar.');
+    }
+}
+
+verificarParOuImpar(2);
+verificarParOuImpar(9);
+verificarParOuImpar(10);
+verificarParOuImpar(21);
